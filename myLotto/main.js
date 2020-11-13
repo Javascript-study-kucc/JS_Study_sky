58e9ddbd3fe13469e3ca2c66b6d6c0ab2ddc9be1
lazy-sky <1cielo@naver.com>
2020-11-14 01:59:07 +0900

    todo init

--- myLotto/main.js
+++ myLotto/main.js
@@ -1,28 +1,25 @@
-const ball1 = document.getElementById("num_1");
-const ball2 = document.getElementById("num_2");
-const ball3 = document.getElementById("num_3");
-const ball4 = document.getElementById("num_4");
-const ball5 = document.getElementById("num_5");
-const ball6 = document.getElementById("num_6");
-const ball7 = document.getElementById("num_bonus");
+const ball1 = document.getElementById("num_1"),
+  ball2 = document.getElementById("num_2"),
+  ball3 = document.getElementById("num_3"),
+  ball4 = document.getElementById("num_4"),
+  ball5 = document.getElementById("num_5"),
+  ball6 = document.getElementById("num_6"),
+  ball7 = document.getElementById("num_bonus");
 
 let generateRandom = () => {
   return Math.floor(Math.random() * 44) + 1;
 };
 
 let getLottoNums = () => {
-  let num;
-  let nums = [];
-  while (nums.length != 7) {
-    num = generateRandom();
-    // 중복 허용x 해야하는데 이상함
-    for (let i = 0; i++; i < nums.length) {
-      if (num == nums[i]) {
-        num = 0;
-      }
-    }
-    nums.push(num);
+  let nums = new Set();
+  while (nums.size != 7) {
+    let num = generateRandom();
+    nums.add(num);
+    // 중복 제거
+    // nums = Array.from(new Set(nums));
   }
+  nums = Array.from(nums);
+
   ball1.innerHTML = nums[0];
   ball2.innerHTML = nums[1];
   ball3.innerHTML = nums[2];
@@ -32,9 +29,6 @@ let getLottoNums = () => {
   ball7.innerHTML = nums[6];
   return nums;
 };
-
-getLottoNums();
-console.log(getLottoNums());
 
 document.getElementById("btn").addEventListener("click", function () {
   console.log(getLottoNums());
---
+++ toDoListWeb/hello.js
@@ -0,0 +1,3 @@
+const form = document.querySelector("form"),
+  input = form.querySelector("input");
+  sayHi = document.querySelector
