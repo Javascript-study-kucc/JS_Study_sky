01d3073401597482fdd0bd0936c5c7f88e658ee0
lazy-sky <1cielo@naver.com>
2020-11-17 23:42:06 +0900

    2020.11.17

--- toDoListWeb/hello.js
+++ toDoListWeb/hello.js
@@ -1,3 +1,27 @@
-const form = document.querySelector("form"),
-  input = form.querySelector("input");
-  sayHi = document.querySelector
+const form = document.querySelector(".js-form"),
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
+
+const user_ls = "currentUser";
+const showing_cn = "showing";
+
+function greeting(text) {
+  form.classList.remove(showing_cn);
+  greeting.classList.add(showing_cn);
+  greeting.innerText = `Hello ${Text}`;
+}
+
+// local storage for user name
+function loadName() {
+  const currentUser = localStorage.getItem(user_ls);
+  if (currentUser === null) {
+  } else {
+    greeting(currentUser);
+  }
+}
+
+function init() {
+  loadName();
+}
+
+init();
