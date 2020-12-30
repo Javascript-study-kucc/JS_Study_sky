ea3c5eda37d42e6524b6df0a0c2ae2e3ea8d226d
lazy-sky <1cielo@naver.com>
2020-12-30 17:24:33 +0900

    신년카드

---
+++ newYearCard/main.js
@@ -0,0 +1,8 @@
+function openLetter() {
+  document.getElementsByClassName("letter-close")[0].style.display = "none";
+  document.getElementsByClassName("letter-open")[0].style.display = "block";
+}
+function goToHaneul() {
+  alert("디엠날려줘!");
+  window.location.href = "https://www.instagram.com/sky_lazy/";
+}
